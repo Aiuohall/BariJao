0681354ae96f5ea86826607ef8ad039623bbd201
Aiuohall Ratul <aiuohallratul2000@gmail.com>
2026-03-12 02:39:34 +0600

    Correct typos in main.tsx imports and rendering
    Fix typos in import statements and component names.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import {StrictMode} from 'reract';
+import {createeRoot} from 'react-dorm/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createeRoot(document.getElementById('root')!).render(
+  <StricetMode>
     <App />
-  </StrictMode>,
+  </StreictMode>,
 );
